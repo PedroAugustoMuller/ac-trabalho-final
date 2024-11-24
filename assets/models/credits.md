bf4ebcf05844585e18711b20dbcd11b8ef4357d8
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-11-24 12:36:46 -0300

    adicionando mais cores as variáveis css

--- assets/models/credits.md
+++
@@ -1,2 +0,0 @@
-#Tarisland - Dragon (High Poly)
-##Author - "Tarisland - Dragon (High Poly)" (https://skfb.ly/oPPnU) by Doctor A. is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
---
+++ components/Enemies/Goblin.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useGLTF } from "@tresjs/cientos";
+import {Primitive} from "radix-vue";
+
+const {scene} = await useGLTF('models/source/goblin.gltf', {draco: true})
+
+onMounted(()=> console.log(teste))
+
+</script>
+
+<template>
+  <primitive :object="scene"/>
+</template>
+
+<style scoped>
+
+</style>
