bcbdc47ec2b7252812677ba03290553ee5587f72
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-12-01 15:12:04 -0300

    corrigindo bg nav

--- components/Navigation/NavigationMenu.vue
+++ components/Navigation/NavigationMenu.vue
@@ -126,13 +126,13 @@ const menus = [
       <NavigationMenuIndicator
           class="data-[state=hidden]:opacity-0 duration-200 data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-40 flex h-[10px] items-end justify-center overflow-hidden transition-[all,transform_250ms_ease]"
       >
-        <div class="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white"/>
+        <div class="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-[var(--secondary-bg)] dark:bg-[var(--secondary-bg-dark)]"/>
       </NavigationMenuIndicator>
     </NavigationMenuList>
 
     <div class="perspective-[2000px] absolute top-full left-0 flex w-full justify-center">
       <NavigationMenuViewport
-          class="dark:bg-[var(--card-bg-dark)] data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[10px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]"
+          class=" bg-[var(--nav-bg)]  dark:bg-[var(--secondary-bg-dark)] data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[10px] transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]"
       />
     </div>
   </NavigationMenuRoot>
